96ea02df312a843264944e17c7754f27541c6ed7
antonsluch <asluch@nes.ru>
2018-04-02 22:37:38 +0300

    интерфейс функции получения результатов

--- testbot/logic.py
+++ testbot/logic.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
 import re
+import asyncio
+from pprint import pprint
+from random import randint
+
+from testbot.settings import settings
+from testbot.data import db
 
 
-def is_ans(ans):
-    pattern = re.compile('[a-z,_]{1,100}[0-9]{1,3}-[0-9]{1,2}')
-    m = pattern.match(ans)
-    if m != None:
-        return m.group() == ans
-    return False
+async def get_user_results(user_id: int):
+    res = await db.users.find_one({"tg_id": user_id}, projection=["tests"])
+    tests = res["tests"]
+    pprint(tests)
 
-def split_ans(ans):
-    pattern = re.compile('[a-z,_]{1,100}')
-    topic = pattern.match(ans).group()
-    num = ans[len(topic):]
-    task, var = num.split('-')
-    return (topic, task, var)
-    
-def increment(ans):
-    topic, task, var = split_ans(ans)
-    return topic+str(int(task)+1)
 
 if __name__ == '__main__':
-    print(split_ans('sss2-99'))
+    async def test():
+        await get_user_results(77513276)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test())
